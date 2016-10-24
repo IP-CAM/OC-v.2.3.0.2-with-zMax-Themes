2aa850c354a276d7facea5e100718396c34b741f
Henry Senerpida Matidios <henrymatids@gmail.com>
2016-10-24 15:09:53 +0800

    initial commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/motorcustomize/admin/');
+define('HTTP_CATALOG', 'http://localhost/motorcustomize/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/motorcustomize/admin/');
+define('HTTPS_CATALOG', 'http://localhost/motorcustomize/');
+
+// DIR
+define('DIR_APPLICATION', 'S:/xampp/htdocs/motorcustomize/admin/');
+define('DIR_SYSTEM', 'S:/xampp/htdocs/motorcustomize/system/');
+define('DIR_IMAGE', 'S:/xampp/htdocs/motorcustomize/image/');
+define('DIR_LANGUAGE', 'S:/xampp/htdocs/motorcustomize/admin/language/');
+define('DIR_TEMPLATE', 'S:/xampp/htdocs/motorcustomize/admin/view/template/');
+define('DIR_CONFIG', 'S:/xampp/htdocs/motorcustomize/system/config/');
+define('DIR_CACHE', 'S:/xampp/htdocs/motorcustomize/system/storage/cache/');
+define('DIR_DOWNLOAD', 'S:/xampp/htdocs/motorcustomize/system/storage/download/');
+define('DIR_LOGS', 'S:/xampp/htdocs/motorcustomize/system/storage/logs/');
+define('DIR_MODIFICATION', 'S:/xampp/htdocs/motorcustomize/system/storage/modification/');
+define('DIR_UPLOAD', 'S:/xampp/htdocs/motorcustomize/system/storage/upload/');
+define('DIR_CATALOG', 'S:/xampp/htdocs/motorcustomize/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'motorcustomize');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/motorcustomize/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/motorcustomize/');
+
+// DIR
+define('DIR_APPLICATION', 'S:/xampp/htdocs/motorcustomize/catalog/');
+define('DIR_SYSTEM', 'S:/xampp/htdocs/motorcustomize/system/');
+define('DIR_IMAGE', 'S:/xampp/htdocs/motorcustomize/image/');
+define('DIR_LANGUAGE', 'S:/xampp/htdocs/motorcustomize/catalog/language/');
+define('DIR_TEMPLATE', 'S:/xampp/htdocs/motorcustomize/catalog/view/theme/');
+define('DIR_CONFIG', 'S:/xampp/htdocs/motorcustomize/system/config/');
+define('DIR_CACHE', 'S:/xampp/htdocs/motorcustomize/system/storage/cache/');
+define('DIR_DOWNLOAD', 'S:/xampp/htdocs/motorcustomize/system/storage/download/');
+define('DIR_LOGS', 'S:/xampp/htdocs/motorcustomize/system/storage/logs/');
+define('DIR_MODIFICATION', 'S:/xampp/htdocs/motorcustomize/system/storage/modification/');
+define('DIR_UPLOAD', 'S:/xampp/htdocs/motorcustomize/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'motorcustomize');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
